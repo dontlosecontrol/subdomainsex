2c14750c8e97f955f81a0db451a3fd07d701eef8
DontLoseControl <admin@ceaseless.dev>
2024-12-15 03:58:36 +0530

    refactor: Simplify enumerate function signature and improve GenServer call timeout configuration

--- lib/subdomains_finder/engine.ex
+++ lib/subdomains_finder/engine.ex
@@ -1,13 +1,13 @@
 defmodule SubdomainsFinder.Engine do
   @callback name() :: String.t()
   @callback enumerate(domain :: String.t(), opts :: keyword()) :: {:ok, [String.t()]} | {:error, term()}
-  
+
   defmacro __using__(_opts) do
     quote do
       @behaviour SubdomainsFinder.Engine
       use GenServer
       require Logger
-      
+
       def start_link(opts) do
         GenServer.start_link(__MODULE__, opts, name: __MODULE__)
       end
@@ -16,16 +16,16 @@ defmodule SubdomainsFinder.Engine do
       def init(opts) do
         rate_limit = SubdomainsFinder.Config.get_rate_limit(name())
         SubdomainsFinder.RateLimit.Worker.set_rate(name(), rate_limit)
-        
+
         {:ok, %{
           opts: opts,
           rate_limit: rate_limit,
           user_agent: SubdomainsFinder.Config.get(:user_agent)
         }}
       end
-
-      def enumerate(domain, opts \\ []) do
-        GenServer.call(__MODULE__, {:enumerate, domain, opts}, 
+      def enumerate(domain, opts \\ [])
+      def enumerate(domain, opts) do
+        GenServer.call(__MODULE__, {:enumerate, domain, opts},
           SubdomainsFinder.Config.get([:timeouts, :engine]))
       end
 
